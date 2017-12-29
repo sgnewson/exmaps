02a4e20560958c76a31dc41f74eccccc0f45d172
scott <scott.g.newson@gmail.com>
2017-12-29 12:58:07 -0700

    tidy up and remove jquery js file

--- js/exmaps_index.js
+++ js/exmaps_index.js
@@ -41,4 +41,20 @@ function initialize() {
     setupGeologyButton(theMap);
 }
 
+function createMap() {
+    var map = new google.maps.Map(document.getElementById("mapCanvas"), constants.initialMapOptions);
+
+    var lastValidCenter = map.getCenter();
+
+    google.maps.event.addListener(map, 'center_changed', function () {
+        if (constants.mapBounds.contains(map.getCenter())) {
+            lastValidCenter = map.getCenter();
+        } else {
+            map.panTo(lastValidCenter);
+        }
+    });
+
+    return map;
+}
+
 google.maps.event.addDomListener(window, 'load', initialize);
