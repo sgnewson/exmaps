24fc5c908a19acfec6c3097bed7d67b2259ae584
scott <scott.g.newson@gmail.com>
2017-12-29 00:45:39 -0700

    tidy up, remove search loading code

--- js/exmaps_index.js
+++ js/exmaps_index.js
@@ -2,30 +2,7 @@
 var geoOverlayIsVisible = true;
 
 var constants = {
-    geogratisUrlPrefix: "http://geogratis.gc.ca/api/en/nrcan-rncan/ess-sst",
-
-    searchButtonDefaultText: "Search",
-    searchButtonSearchingText: "Searching...",
     selectionRectangleColor: "#FF0000",
-
-    geoGratisSearchCategories: [
-        {
-            humanReadable: "GSC 'A' series",
-            machineReadable: "/-/(urn:iso:series)geological-survey-of-canada-a-series-map"
-        },
-        {
-            humanReadable: "GSC open file",
-            machineReadable: "/-/(urn:iso:series)geological-survey-of-canada-open-file"
-        },
-        {
-            humanReadable: "GSC preliminary map",
-            machineReadable: "/-/(urn:iso:series)geological-survey-of-canada-preliminary-map"
-        },
-        {
-            humanReadable: "GSC Canadian geosciences map",
-            machineReadable: "/-/(urn:iso:series)geological-survey-of-canada-canadian-geoscience-map"
-        }
-    ],
 
     mapBounds: new google.maps.LatLngBounds(
         new google.maps.LatLng(49, -130),
@@ -56,16 +33,13 @@ var constants = {
 
 function initialize() {
     theMap = createMap();
-    activateGeoGratisSearch(theMap);
+    setupSelectionRectangle(theMap);
 
     var loadInteractiveLayers = false;
     loadLayers(theMap, loadInteractiveLayers);
     loadLabels(theMap);
 
     setupGeologyButton(theMap);
-
-    $(".latLngInputBox").change(updateSelectionBoxFromInputs);
-    $("#searchButton").click(loadGeoGratis);
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
