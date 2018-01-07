4856f6bcfc20e3be3d8c491060183fa4f1d51e60
Scott Newson <scott.g.newson@gmail.com>
2018-01-06 22:52:14 -0700

    add separate search for Canada without geology overlay

--- js/index.js
+++ js/index.js
@@ -1,34 +1,4 @@
-﻿var constants = {
-    selectionRectangleColor: "#FF0000",
-
-    mapBounds: new google.maps.LatLngBounds(
-        new google.maps.LatLng(49, -130),
-        new google.maps.LatLng(60, -110)),
-
-    initialMapOptions: {
-        zoom: 7,
-        maxZoom: 9,
-        minZoom: 5,
-        center: new google.maps.LatLng(52.5, -116),
-        mapTypeId: google.maps.MapTypeId.TERRAIN,
-        draggable: false,
-        scrollwheel: false,
-        disableDoubleClickZoom: true,
-        disableDefaultUI: false,
-        keyboardShortcuts: true,
-        styles: [
-            {
-                featureType: "poi",
-                elementType: "labels",
-                stylers: [
-                  { visibility: "off" }
-                ]
-            }
-        ]
-    }
-};
-
-function initialize() {
+﻿function initialize() {
     $("#gscASeriesButton").prop('disabled', true);
     $("#gscPreliminaryButton").prop('disabled', true);
     $("#gscPapersButton").prop('disabled', true);
@@ -37,10 +7,12 @@ function initialize() {
     setupSelectionRectangle(map);
     urlSearchParams(map);
 
-    loadLayers(map);
-    loadLabels(map);
+    if (constants.showGeology) {
+        loadLayers(map);
+        loadLabels(map);
+        setupGeologyButton(map);
+    }
 
-    setupGeologyButton(map);
     setExmapsLinkDefault();
 
     $("#gscASeriesButton").click(openUrl("GSCMAP-A"));
