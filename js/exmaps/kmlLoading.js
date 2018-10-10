d8590196edd6465d1857daf36b650fa7a47b59e3
Scott Newson <scott.g.newson@gmail.com>
2018-10-09 18:53:28 -0600

    update kmz links

--- js/exmaps/kmlLoading.js
+++ js/exmaps/kmlLoading.js
@@ -6,19 +6,19 @@ var alwaysOnLayers = [];
 
 function loadLayers(googleMap) {
     var geologyLayer = {
-        url: "https://www.dropbox.com/s/ngv790iaeihljgu/ExMaps%20surface%20geology.kmz?dl=1",
+        url: "https://www.dropbox.com/s/91hkwlxgyoj7r2j/ExMaps%20surface%20geology.kmz?dl=1",
         clickable: false,
         toggleable: true
     };
 
     var lineLayer = {
-        url: "https://www.dropbox.com/s/l5cpzipm1oavjoy/ExMaps%20line%20geology.kmz?dl=1",
+        url: "https://www.dropbox.com/s/tmnqu8lt50dmb3g/ExMaps%20line%20geology.kmz?dl=1",
         clickable: false,
         toggleable: true
     };
 
     var fieldsLayer = {
-        url: "https://www.dropbox.com/s/pwgvddsvsdxz1t8/ExMaps%20field%20outlines.kmz?dl=1",
+        url: "https://www.dropbox.com/s/qi8cbahg7m1bs9t/ExMaps%20field%20outlines.kmz?dl=1",
         clickable: false,
         toggleable: false
     };
